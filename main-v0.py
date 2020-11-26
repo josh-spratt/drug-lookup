b013814598ae1379949a3b90f32c36984ea57d87
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-11-25 21:44:19 -0600

    beginning argparse exploration

--- main-v0.py
+++ main-v0.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 import argparse
 
 
+def get_list_of_potential_drug_matches(key, drug):
+    # Sends a drug param to rest rx api and returns a json response body
+    url = 'https://api.seer.cancer.gov/rest/rx/latest'
+    headers = {'X-SEERAPI-Key': key}
+    params = {'count': '100', 'q': drug}
+    req = requests.get(url, headers=headers, params=params)
+    json_response = req.json()
+    return json_response
+
+
 def generate_score_histogram_from_json_request(response_data):
     # Generates a histogram that is the distribution of result match scores
     score_list = [int(result_dict['score'])
@@ -15,13 +25,11 @@ def generate_score_histogram_from_json_request(response_data):
     plt.show()
 
 
-# Global variables
-url = 'https://api.seer.cancer.gov/rest/rx/latest'
-api_key = input('enter api key...\n')
-drug = input('enter drug name\n')
-headers = {'X-SEERAPI-Key': api_key}
-params = {'count': '100', 'q': drug}
-req = requests.get(url, headers=headers, params=params)
-json_response = req.json()
-
-generate_score_histogram_from_json_request(json_response)
+parser = argparse.ArgumentParser()
+parser.add_argument("--key")
+parser.add_argument("--drug")
+args = parser.parse_args()
+print(args.key)
+print(args.drug)
+"""get_list_of_potential_drug_matches()
+generate_score_histogram_from_json_request(json_response)"""
