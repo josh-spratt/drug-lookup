c9baa584e6031d055ff8861892356b1009c4074f
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-13 00:54:29 -0600

    working but rough first run at user result select

--- main-v0.py
+++ main-v0.py
@@ -65,24 +65,47 @@ def get_rx_entry_from_id(key, list_of_max_scores):
 
 
 def parse_desired_drug_data_from_json_response(resultant_dict):
+    def check_dict_for_key(dict, key):
+        if key in dict.keys():
+            print(key)
+            print(dict[key])
+        else:
+            print(f"{key} not found")
     user_content_depth_selection = (input('What data are you looking for?\n1.) Very detailed drug information\n2.)'
                                           ' A broad overview of this drug\n3.) Alternate names for this drug\n4.) '
-                                          'Primary site & histology information\n5.) Remarks, including recent FDA '
-                                          'approvals, news, and history\n6.) Drug categorization'))
-    if (user_content_depth_selection == 1 or user_content_depth_selection == '1.' or user_content_depth_selection
-            == '1.)'or user_content_depth_selection == '1)' or user_content_depth_selection.lower() == 'one'):
-        name = resultant_dict['name']
-        print(name)
-        alternate_name = resultant_dict['alternate_name']
-        print(alternate_name)
-        category = resultant_dict['category']
-        print(category)
-        subcategory = resultant_dict['subcategory']
-        print(subcategory)
-        primary_site = resultant_dict['primary_site']
-        print(primary_site)
-        remarks = resultant_dict['remarks']
-        print(remarks)
+                                          'Primary site information\n5.) Remarks, including recent FDA '
+                                          'approvals, news, and history\n6.) Drug categorization\n'))
+    if (user_content_depth_selection == '1' or user_content_depth_selection == '1.' or user_content_depth_selection
+            == '1.)' or user_content_depth_selection == '1)' or user_content_depth_selection.lower() == 'one'):
+        check_dict_for_key(resultant_dict, 'name')
+        check_dict_for_key(resultant_dict, 'alternate_name')
+        check_dict_for_key(resultant_dict, 'category')
+        check_dict_for_key(resultant_dict, 'subcategory')
+        check_dict_for_key(resultant_dict, 'primary_site')
+        check_dict_for_key(resultant_dict, 'remarks')
+        #check_dict_for_key(resultant_dict, 'histology')
+    if (user_content_depth_selection == '2' or user_content_depth_selection == '2.' or user_content_depth_selection
+            == '2.)' or user_content_depth_selection == '2)' or user_content_depth_selection.lower() == 'two'):
+        check_dict_for_key(resultant_dict, 'name')
+        check_dict_for_key(resultant_dict, 'category')
+        check_dict_for_key(resultant_dict, 'primary_site')
+    if (user_content_depth_selection == '3' or user_content_depth_selection == '3.' or user_content_depth_selection
+            == '3.)' or user_content_depth_selection == '3)' or user_content_depth_selection.lower() == 'three'):
+        check_dict_for_key(resultant_dict, 'name')
+        check_dict_for_key(resultant_dict, 'alternate_name')
+    if (user_content_depth_selection == '4' or user_content_depth_selection == '4.' or user_content_depth_selection
+            == '4.)' or user_content_depth_selection == '4)' or user_content_depth_selection.lower() == 'four'):
+        check_dict_for_key(resultant_dict, 'name')
+        check_dict_for_key(resultant_dict, 'primary_site')
+    if (user_content_depth_selection == '5' or user_content_depth_selection == '5.' or user_content_depth_selection
+            == '5.)' or user_content_depth_selection == '5)' or user_content_depth_selection.lower() == 'five'):
+        check_dict_for_key(resultant_dict, 'name')
+        check_dict_for_key(resultant_dict, 'remarks')
+    if (user_content_depth_selection == '6' or user_content_depth_selection == '6.' or user_content_depth_selection
+            == '6.)' or user_content_depth_selection == '6)' or user_content_depth_selection.lower() == 'six'):
+        check_dict_for_key(resultant_dict, 'name')
+        check_dict_for_key(resultant_dict, 'category')
+        check_dict_for_key(resultant_dict, 'subcategory')
 
 
 parser = argparse.ArgumentParser()
@@ -102,5 +125,4 @@ else:
 best_match = find_max_match_scores(matches)
 user_verifies_max_score_correctness(best_match)
 dict_of_specific_drug_result = get_rx_entry_from_id(args.key, best_match)
-#print(json.dumps(dict_of_specific_drug_result, indent=2))
 parse_desired_drug_data_from_json_response(dict_of_specific_drug_result)
